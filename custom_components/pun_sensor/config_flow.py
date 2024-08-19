16f99e07f5a092a965c8939f4ef3723f18ddddd3
StefanoGiu <35804888+StefanoGiu@users.noreply.github.com>
2024-08-19 08:16:08 +0200

    Update config_flow.py

--- custom_components/pun_sensor/config_flow.py
+++ custom_components/pun_sensor/config_flow.py
@@ -1,76 +1,59 @@
-from homeassistant import config_entries
-from homeassistant.core import callback
-from homeassistant.data_entry_flow import FlowResult
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-from .const import (
-    DOMAIN,
-    CONF_SCAN_HOUR,
-    CONF_ACTUAL_DATA_ONLY,
-)
 
-class PUNOptionsFlow(config_entries.OptionsFlow):
-    """Opzioni per prezzi PUN (= riconfigurazione successiva)"""
-
-    def __init__(self, entry: config_entries.ConfigEntry) -> None:
-        """Inizializzazione options flow"""
-        self.config_entry = entry
-
-    async def async_step_init(self, user_input=None) -> FlowResult:
-        """Gestisce le opzioni"""
+    async def async_step_step3(self, user_input=None):
+        """Gestione prima configurazione da Home Assistant"""
+        self.data.update(user_input)
         errors = {}
-        if user_input is not None:
-            # Configurazione valida (validazione integrata nello schema)
-            return self.async_create_entry(
-                title='PUN',
-                data=user_input
-            )
-
-        # Schema dati di opzione (con default sui valori attuali)
         data_schema = {
-            vol.Required(CONF_SCAN_HOUR, default=self.config_entry.options.get(CONF_SCAN_HOUR, self.config_entry.data[CONF_SCAN_HOUR])): vol.All(cv.positive_int, vol.Range(min=0, max=23)),
-            vol.Optional(CONF_ACTUAL_DATA_ONLY, default=self.config_entry.options.get(CONF_ACTUAL_DATA_ONLY, self.config_entry.data[CONF_ACTUAL_DATA_ONLY])): cv.boolean,
+            vol.Required(CONF_ASOS_SC1, default=0.029809) : cv.positive_float,
+            vol.Required(CONF_ASOS_SC2, default=0.029809) : cv.positive_float,
+            vol.Required(CONF_ARIM_SC1, default=0.008828) : cv.positive_float,
+            vol.Required(CONF_ARIM_SC2, default=0.008828) : cv.positive_float,
         }
-
         # Mostra la schermata di configurazione, con gli eventuali errori
         return self.async_show_form(
-            step_id="init", data_schema=vol.Schema(data_schema), errors=errors
+            step_id="step4", data_schema=vol.Schema(data_schema), errors=errors, 
         )
-    
-class PUNConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Configurazione per prezzi PUN (= prima configurazione)"""
 
-    # Versione della configurazione (per utilizzi futuri)
-    VERSION = 1
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(entry: config_entries.ConfigEntry) -> PUNOptionsFlow:
-        """Ottiene le opzioni per questa configurazione"""
-        return PUNOptionsFlow(entry)
-
-    async def async_step_user(self, user_input=None):
+    async def async_step_step4(self, user_input=None):
         """Gestione prima configurazione da Home Assistant"""
-        # Controlla che l'integrazione non venga eseguita più volte
-        await self.async_set_unique_id(DOMAIN)
-        self._abort_if_unique_id_configured()
-
+        self.data.update(user_input)
         errors = {}
-        if user_input is not None:
-            # Configurazione valida (validazione integrata nello schema)
-            return self.async_create_entry(
-                title='PUN',
-                data=user_input
-            )
-
-        # Schema dati di configurazione (con default fissi)
         data_schema = {
+            vol.Required(CONF_ACCISA_TAX, default=0.022700) : cv.positive_float,
+            vol.Required(CONF_IVA, default=10) : vol.All(cv.positive_int, vol.Range(min=0, max=100)),
+            vol.Required(CONF_DISCOUNT, default=1) : cv.positive_float,
+            vol.Required(CONF_TV_TAX, default=7) : cv.positive_float,
+        }
+        # Mostra la schermata di configurazione, con gli eventuali errori
+        return self.async_show_form(
+            step_id="step5", data_schema=vol.Schema(data_schema), errors=errors, 
+        )
+
+    async def async_step_step5(self, user_input=None):
+        """Gestione prima configurazione da Home Assistant"""
+        self.data.update(user_input)
+        errors = {}
+        data_schema = {
+            vol.Required(CONF_MONTHY_ENTITY_SENSOR) : selector.selector({
+				"entity": {
+					"multiple": "false",
+				    "filter": [{"domain" : "sensor", "device_class" : "energy"}],
+				}
+			}),
             vol.Required(CONF_SCAN_HOUR, default=1): vol.All(cv.positive_int, vol.Range(min=0, max=23)),
             vol.Optional(CONF_ACTUAL_DATA_ONLY, default=False): cv.boolean,
         }
-
         # Mostra la schermata di configurazione, con gli eventuali errori
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(data_schema), errors=errors
+            step_id="step6", data_schema=vol.Schema(data_schema), errors=errors, 
         )
 
+    async def async_step_step6(self, user_input=None):
+        """Gestione prima configurazione da Home Assistant"""
+        self.data.update(user_input)
+        if user_input is not None:
+            # Configurazione valida (validazione integrata nello schema)
+            return self.async_create_entry(
+                title='Bolletta',
+                data=self.data
+            )
